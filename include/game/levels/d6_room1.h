372112ca33ab870ddb0100489503f67c6297cfd9
McLean Baran <baranmcl@gmail.com>
2026-06-12 00:01:20 -0500

    test(content): ground dungeon-6 content on the floor row + co-locate entrances with return doors

--- include/game/levels/d6_room1.h
+++ include/game/levels/d6_room1.h
@@ -2,16 +2,16 @@
 #include "logic/level_data.h"
 
 inline constexpr unsigned char D6_ROOM1_TILES[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-inline constexpr logic::EntitySpawn D6_ROOM1_ENEMIES[] = { {6,18,3,9,0}, {8,18,4,12,0} };
+inline constexpr logic::EntitySpawn D6_ROOM1_ENEMIES[] = { {7,19,5,10,0}, {9,19,6,12,0} };
 inline constexpr logic::GateSpawn D6_ROOM1_GATES[] = { {0,0,logic::GateType::Gap} };
 inline constexpr logic::DoorSpawn D6_ROOM1_DOORS[] = { {0,0,0} };
 inline constexpr logic::AbilityPickup D6_ROOM1_PICKUPS[] = { {0,0,logic::Ability::Fire} };
 inline constexpr logic::BlockSpawn D6_ROOM1_BLOCKS[] = { {0,0} };
 inline constexpr logic::PlateSpawn D6_ROOM1_PLATES[] = { {0,0,0,0} };
 inline constexpr logic::ButtonSpawn D6_ROOM1_BUTTONS[] = { {0,0,0,0} };
-inline constexpr logic::BrazierSpawn D6_ROOM1_BRAZIERS[] = { {10,18,0} };
+inline constexpr logic::BrazierSpawn D6_ROOM1_BRAZIERS[] = { {11,19,0} };
 inline constexpr logic::BrazierGroupSpawn D6_ROOM1_BRAZIER_GROUPS[] = { {1,16,1,0} };
-inline constexpr logic::EntranceSpawn D6_ROOM1_ENTRANCES[] = { {0,3,18,1} };
-inline constexpr logic::RoomDoorSpawn D6_ROOM1_ROOM_DOORS[] = { {13,18,0,1} };
-inline constexpr logic::LevelData D6_ROOM1_DATA = { D6_ROOM1_TILES, 32, 22, 4, 18, true, 22, 18, true, 27, 18, D6_ROOM1_ENEMIES, 2, D6_ROOM1_GATES, 0, D6_ROOM1_DOORS, 0, D6_ROOM1_PICKUPS, 0, D6_ROOM1_BLOCKS, 0, D6_ROOM1_PLATES, 0, D6_ROOM1_BUTTONS, 0, D6_ROOM1_BRAZIERS, 1, D6_ROOM1_BRAZIER_GROUPS, 1, D6_ROOM1_ENTRANCES, 1, D6_ROOM1_ROOM_DOORS, 1 };
+inline constexpr logic::EntranceSpawn D6_ROOM1_ENTRANCES[] = { {0,3,19,1} };
+inline constexpr logic::RoomDoorSpawn D6_ROOM1_ROOM_DOORS[] = { {4,19,0,1} };
+inline constexpr logic::LevelData D6_ROOM1_DATA = { D6_ROOM1_TILES, 32, 22, 5, 19, true, 22, 19, true, 27, 19, D6_ROOM1_ENEMIES, 2, D6_ROOM1_GATES, 0, D6_ROOM1_DOORS, 0, D6_ROOM1_PICKUPS, 0, D6_ROOM1_BLOCKS, 0, D6_ROOM1_PLATES, 0, D6_ROOM1_BUTTONS, 0, D6_ROOM1_BRAZIERS, 1, D6_ROOM1_BRAZIER_GROUPS, 1, D6_ROOM1_ENTRANCES, 1, D6_ROOM1_ROOM_DOORS, 1 };
 
